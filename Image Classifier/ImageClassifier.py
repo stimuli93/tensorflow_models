01d4b5789068b9d5a5fba59f28046d811f0db503
stimuli93 <saurabh.arora67@gmail.com>
2016-08-27 03:41:33 +0530

    added model persistence support

--- Image Classifier/ImageClassifier.py
+++ Image Classifier/ImageClassifier.py
@@ -1,11 +1,12 @@
 import tensorflow as tf
 import numpy as np
+import os
 
 
 class ConvolutionalClassifier(object):
     def __init__(self, n_classes=2):
 
-        img_dims = (256, 256, 3)
+        img_dims = (128, 128, 3)
         self.weights = self._initialize_weights(img_channels=img_dims[2], n_classes=n_classes)
         self.x = tf.placeholder(tf.float32, shape=[None, img_dims[0], img_dims[1], img_dims[2]], name="x_image")
         self.y = tf.placeholder(tf.float32, shape=[None, n_classes])
@@ -17,13 +18,13 @@ class ConvolutionalClassifier(object):
         h_conv3 = tf.nn.relu(self.conv2d(h_pool2, self.weights['W_conv3']) + self.weights['b_conv3'])
         h_pool3 = self.max_pool_2x2(h_conv3)
 
-        h_pool3_flat = tf.reshape(h_pool3, [-1, 32*32*32])
+        h_pool3_flat = tf.reshape(h_pool3, [-1, 16*16*32])
         h_fc1 = tf.nn.relu(tf.matmul(h_pool3_flat, self.weights['W_fc1']) + self.weights['b_fc1'])
         y_pred = tf.nn.softmax(tf.matmul(h_fc1, self.weights['W_fc2']) + self.weights['b_fc2'])
         self.cost = tf.reduce_mean(-tf.reduce_sum(self.y * tf.log(y_pred), reduction_indices=[1]))
         correct_prediction = tf.equal(tf.argmax(y_pred, 1), tf.argmax(self.y, 1))
         self.accuracy = tf.reduce_mean(tf.cast(correct_prediction, tf.float32))
-        self.sess = tf.Session()
+        self.sess = tf.InteractiveSession()
 
         cost_summ = tf.scalar_summary("loss ", self.cost)
         accuracy_summary = tf.scalar_summary("accuracy", self.accuracy)
@@ -31,6 +32,15 @@ class ConvolutionalClassifier(object):
         # Merge all the summaries and write them out to /tmp/convClf_logs
         self.merged = tf.merge_all_summaries()
         self.writer = tf.train.SummaryWriter("/tmp/convClf_logs", self.sess.graph)
+
+        ckpt_dir = "./ckpt_dir"
+        if not os.path.exists(ckpt_dir):
+            os.makedirs(ckpt_dir)
+
+        self.global_step = tf.Variable(0, name='global_step', trainable=False)
+
+        # Call this after declaring all tf.Variables.
+        self.saver = tf.train.Saver()
 
         init = tf.initialize_all_variables()
         self.sess.run(init)
@@ -43,9 +53,9 @@ class ConvolutionalClassifier(object):
         all_weights['b_conv2'] = tf.Variable(tf.constant(0.1, shape=[16]))
         all_weights['W_conv3'] = tf.Variable(tf.truncated_normal(shape=[3, 3, 16, 32], stddev=0.1))
         all_weights['b_conv3'] = tf.Variable(tf.constant(0.1, shape=[32]))
-        all_weights['W_fc1'] = tf.Variable(tf.truncated_normal(shape=[32*32*32, 512], stddev=0.1))
-        all_weights['b_fc1'] = tf.Variable(tf.constant(0.1, shape=[512]))
-        all_weights['W_fc2'] = tf.Variable(tf.truncated_normal(shape=[512, n_classes], stddev=0.1))
+        all_weights['W_fc1'] = tf.Variable(tf.truncated_normal(shape=[16*16*32, 64], stddev=0.1))
+        all_weights['b_fc1'] = tf.Variable(tf.constant(0.1, shape=[64]))
+        all_weights['W_fc2'] = tf.Variable(tf.truncated_normal(shape=[64, n_classes], stddev=0.1))
         all_weights['b_fc2'] = tf.Variable(tf.constant(0.1, shape=[n_classes]))
 
         return all_weights
@@ -61,7 +71,7 @@ class ConvolutionalClassifier(object):
         result = self.sess.run([self.accuracy, self.cost], feed_dict={self.x: X, self.y: y})
         return result
 
-    def train(self, trX, trY, learning_rate=1e-3, batch_size=10, n_iters=100, start=0):
+    def train(self, trX, trY, learning_rate=1e-3, batch_size=10, n_iters=100):
 
         train_step = tf.train.AdamOptimizer(learning_rate).minimize(self.cost)
         uninitialized_vars = []
@@ -74,12 +84,22 @@ class ConvolutionalClassifier(object):
         init_new_vars_op = tf.initialize_variables(uninitialized_vars)
         self.sess.run(init_new_vars_op)
 
+        ckpt_dir = "./ckpt_dir"
+        ckpt = tf.train.get_checkpoint_state(ckpt_dir)
+        if ckpt and ckpt.model_checkpoint_path:
+            print(ckpt.model_checkpoint_path)
+            self.saver.restore(self.sess, ckpt.model_checkpoint_path)  # restore all variables
+
+        start = self.global_step.eval()  # get last global_step
         for i in xrange(n_iters):
             batch = np.random.randint(trX.shape[0], size=batch_size)
             self.sess.run(train_step, feed_dict={self.x: trX[batch], self.y: trY[batch]})
             if i % 10 == 0:
-                result = self.sess.run([self.merged, self.accuracy], feed_dict={self.x: trX[batch], self.y: trY[batch]})
+                result = self.sess.run([self.merged, self.cost], feed_dict={self.x: trX[batch], self.y: trY[batch]})
                 summary_str = result[0]
-                acc = result[1]
+                loss = result[1]
                 self.writer.add_summary(summary_str, i+start)
-                print("Accuracy at step %s: %s" % (i+start, acc))
+                print("Loss at step %s: %s" % (i+start, loss))
+                if i % 100 == 0:
+                    self.global_step.assign(i + start).eval()  # set and update(eval) global_step with index, i
+                    self.saver.save(self.sess, "./ckpt_dir/model.ckpt", global_step=self.global_step)
